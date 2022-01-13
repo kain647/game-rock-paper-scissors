34dedf5602858324b3c663e441c24eb9a38545ca
Aleksey <aleksey.amelchenko@gmail.com>
2022-01-13 17:15:52 +0300

    Add language en-ru

---
+++ src/local.js
@@ -0,0 +1,20 @@
+export const labels = {
+    "en": {
+        "win": "YOU WIN!",
+        "lose": "Lose!",
+        "draw": "Draw!",
+        "score": "Score",
+        "picked": "you picked",
+        "robotPicked": "robot picked",
+        "playAgain": "play again"
+    },
+    "ru": {
+        "win": "Победа!",
+        "lose": "Проигрыш",
+        "draw": "Ничья",
+        "score": "Счет",
+        "picked": "Ваш выбор",
+        "robotPicked": "Выбор компьютера",
+        "playAgain": "Играть еще раз"
+    }
+}
